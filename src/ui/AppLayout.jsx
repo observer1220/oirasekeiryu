72857af8ffa0841940b3a14969a511f980e49ebf
observer1220 <observer1220@gmail.com>
2024-03-28 16:05:13 +0800

    deploy failure debug #2

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
+import { styled } from "styled-components";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Sidebar from "./SideBar";
-import { styled } from "styled-components";
+import Sidebar from "./Sidebar";
 
 const StyledAppLayout = styled.div`
   display: grid;
