f188a62a3c5d8a25d11309be0afa3a822c98c051
observer1220 <observer1220@gmail.com>
2024-07-05 15:11:41 +0800

    deploy 2024-07-05 15:11:41

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
-  base: process.env.NODE_ENV === 'production' ? '/oirasekeiryu/' : '/',
+export default defineConfig( () => {
+  const env = loadEnv("", process.cwd());
+  return {
+    plugins: [react(), eslint()],
+    base: env.VITE_BASE,
+  }
 })
