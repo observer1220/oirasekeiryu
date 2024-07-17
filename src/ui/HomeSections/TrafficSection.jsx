9d64394b725bf4a3f6b5ee81826794cab22b2782
observer1220 <observer1220@gmail.com>
2024-07-17 15:11:54 +0800

    deploy 2024-07-17 15:11:54

--- src/ui/HomeSections/TrafficSection.jsx
+++ src/ui/HomeSections/TrafficSection.jsx
@@ -3,28 +3,33 @@ import { forwardRef } from "react";
 
 const TextLayer = styled.div`
   display: grid;
+  grid-template-columns: 1fr 1fr;
   align-items: center;
-  font-weight: bold;
-  margin: 4rem;
+  margin: 60px 0;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const GoogleMap = styled.iframe`
+  width: 600px;
+  height: 450px;
+  border: 0;
 `;
 
 const TrafficSection = forwardRef((props, ref) => {
   return (
     <TextLayer ref={ref}>
       <h1>交通指南</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        ultricies, nunc id ultricies ultricies, justo odio ultricies libero, ac
-        tincidunt enim ligula ut eros. Nullam nec lacus consectetur, ultricies
-        purus ac, tincidunt libero. Nulla facilisi. Sed auctor, mi vel tincidunt
-        luctus, mi sapien ultricies nunc, nec fermentum nisi dolor nec turpis.
-        Nullam nec lacus consectetur, ultricies purus ac, tincidunt libero.
-        Nulla facilisi. Sed auctor, mi vel tincidunt luctus, mi sapien ultricies
-        nunc, nec fermentum nisi dolor nec turpis.
-      </p>
+      <GoogleMap
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3030.4543980147737!2d140.9791532760189!3d40.57572437141449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f9b15241784510f%3A0x6958fecded1ea4ce!2z5pif6YeO6ZuG5ZyYIOWlp-WFpeeAqOa6qua1gemFkuW6lw!5e0!3m2!1szh-TW!2stw!4v1721199577090!5m2!1szh-TW!2stw"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
     </TextLayer>
   );
 });
 
-TrafficSection.displayName = 'TrafficSection';
+TrafficSection.displayName = "TrafficSection";
 export default TrafficSection;
