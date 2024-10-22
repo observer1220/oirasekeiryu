8a7c91751c85bdff2c3f0f7d4589175bf937d7a4
observer1220 <observer1220@gmail.com>
2024-10-22 14:47:04 +0800

    deploy 2024-10-22 14:47:04

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_KEY as string;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  console.log("mode", mode);
-
-  return {
-    plugins: [react(), checker({ typescript: true })],
-    define: {
-      "process.env": env,
-    },
-    base: env.VITE_BASE_URL,
-  };
+export default defineConfig({
+  plugins: [react(), checker({ typescript: true })],
+  base:
+    process.env.NODE_ENV === "development" ? "/oirasekeiryu" : "oirasekeiryu",
 });
