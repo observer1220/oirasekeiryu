821643be005b0ecfd65554bfb7b3f50080b8e864
observer1220 <observer1220@gmail.com>
2024-10-22 14:01:26 +0800

    deploy 2024-10-22 14:01:26

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
-export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
-  base:
-    process.env.NODE_ENV === "development" ? "/oirasekeiryu" : "oirasekeiryu",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), checker({ typescript: true })],
+    base:
+      process.env.NODE_ENV === "development" ? "/oirasekeiryu" : "oirasekeiryu",
+    define: {
+      "process.env": env,
+    },
+  };
 });
